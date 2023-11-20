2539f2a730ba889aba6605499776bfa37e0f77c3
AlexNottaBen <alex.nottaben@gmail.com>
2023-11-20 11:38:08 +0200

    Formatted with pep8.

--- Online Clothing Store/main.py
+++ Online Clothing Store/main.py
@@ -3,6 +3,7 @@
 
 from abc import ABC, abstractmethod
 
+
 class AdditionalItemsImplementation(ABC):
     @abstractmethod
     def get_same_type_items(self, product_type):
@@ -12,28 +13,35 @@ class AdditionalItemsImplementation(ABC):
     def get_same_collection_items(self, collection):
         pass
 
+
 class AdditionalItemsImplementationConcrete(AdditionalItemsImplementation):
     def get_same_type_items(self, product_type):
-        # The logic of obtaining other goods of the same type
-        pass
+        print("The logic of obtaining other goods of the same type")
 
     def get_same_collection_items(self, collection):
-        # The logic of obtaining other products from the same collection
-        pass
+        print("The logic of obtaining other products from the same collection")
+
 
 class ProductPage:
     def __init__(self, additional_items_implementation: AdditionalItemsImplementation):
         self._additional_items_implementation = additional_items_implementation
 
     def show_additional_items(self, product_type, collection):
-        same_type_items = self._additional_items_implementation.get_same_type_items(product_type)
-        same_collection_items = self._additional_items_implementation.get_same_collection_items(collection)
+        same_type_items = self._additional_items_implementation.get_same_type_items(
+            product_type
+        )
+        same_collection_items = (
+            self._additional_items_implementation.get_same_collection_items(collection)
+        )
 
-        # The logic of displaying additional products on the page
-        pass
+        print("The logic of displaying additional products on the page")
+
 
 additional_items_implementation = AdditionalItemsImplementationConcrete()
 product_page = ProductPage(additional_items_implementation)
+
+product_type = ""
+collection = ""
 
 # Call the function to display additional products
 product_page.show_additional_items(product_type, collection)
