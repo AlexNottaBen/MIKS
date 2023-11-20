56908e7d4e5fc154ce274edc8c3251d80baefff5
AlexNottaBen <alex.nottaben@gmail.com>
2023-11-20 16:08:30 +0200

    Added Frontend for Task 1.

--- Online Clothing Store/main.py
+++ Online Clothing Store/main.py
@@ -1,47 +1,30 @@
 #! /usr/bin/python3
 # -*- coding: utf-8 -*-
 
-from abc import ABC, abstractmethod
+from flask import Flask, render_template, request
+# from flask_sqlalchemy import SQLAlchemy
 
 
-class AdditionalItemsImplementation(ABC):
-    @abstractmethod
-    def get_same_type_items(self, product_type):
-        pass
-
-    @abstractmethod
-    def get_same_collection_items(self, collection):
-        pass
+app = Flask(
+    import_name=__name__,
+    template_folder="templates",
+    static_folder="static"
+)
 
 
-class AdditionalItemsImplementationConcrete(AdditionalItemsImplementation):
-    def get_same_type_items(self, product_type):
-        print("The logic of obtaining other goods of the same type")
-
-    def get_same_collection_items(self, collection):
-        print("The logic of obtaining other products from the same collection")
+@app.route("/")
+def index():
+    return render_template("index.html")
 
 
-class ProductPage:
-    def __init__(self, additional_items_implementation: AdditionalItemsImplementation):
-        self._additional_items_implementation = additional_items_implementation
-
-    def show_additional_items(self, product_type, collection):
-        same_type_items = self._additional_items_implementation.get_same_type_items(
-            product_type
-        )
-        same_collection_items = (
-            self._additional_items_implementation.get_same_collection_items(collection)
-        )
-
-        print("The logic of displaying additional products on the page")
+@app.route("/product/<int:id>")
+def product_page(id: int):
+    return render_template("product.html")
 
 
-additional_items_implementation = AdditionalItemsImplementationConcrete()
-product_page = ProductPage(additional_items_implementation)
+def main():
+    app.run(debug=True, port=5000)
 
-product_type = ""
-collection = ""
 
-# Call the function to display additional products
-product_page.show_additional_items(product_type, collection)
+if __name__ == "__main__":
+    main()
