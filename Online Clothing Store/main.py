0f24ee53a598a610c45f4f18b6a673d4f235568f
AlexNottaBen <alex.nottaben@gmail.com>
2023-11-22 20:36:29 +0200

    Implemented Bridge pattern.

--- Online Clothing Store/main.py
+++ Online Clothing Store/main.py
@@ -5,8 +5,10 @@ from flask import render_template, request, redirect
 
 from app import app
 from Product import Product
-from ProductPhoto import ProductPhoto
 from Purchase import Purchase
+from ProductPage import ProductPage
+from ProductPhoto import ProductPhoto
+from AdditionalProducts import AdditionalProducts
 
 
 @app.route("/")
@@ -18,25 +20,14 @@ def index():
 
 @app.route("/product/<int:id>")
 def product_page(id: int):
-    try:
-        product = Product.query.filter_by(id=id).first()
-        photos = ProductPhoto.query.all()
-        other_products_type = Product.query.filter_by(
-            type_of_product=product.type_of_product
-        )
-        other_products_collection = Product.query.filter_by(
-            collection=product.collection
-        )
-        return render_template(
-            "product.html",
-            product=product,
-            photos=photos,
-            other_products_type=other_products_type,
-            other_products_collection=other_products_collection,
-        )
-    except (AttributeError, UnboundLocalError) as exception:
-        print(exception)
-        return redirect("/")
+    product = Product.query.filter_by(id=id).first_or_404()
+    photos = ProductPhoto.query.all()
+
+    additional_products: AdditionalProducts = AdditionalProducts()
+    product_page: ProductPage = ProductPage(additional_products)
+
+    # Call the function to display additional products
+    return product_page.show_additional_products(product, photos)
 
 
 @app.route("/buy")
--- Online Clothing Store/migrate.py
+++
@@ -1,7 +0,0 @@
-from app import app, database
-from Product import Product
-from ProductPhoto import ProductPhoto
-
-
-app.app_context().push()
-database.create_all()
