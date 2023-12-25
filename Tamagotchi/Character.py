575d50f0cdf65bfbaf5f46117217c6d81106c038
AlexNottaBen <alex.nottaben@gmail.com>
2023-12-25 22:25:40 +0200

    Implemented Christmas bundle.

--- Tamagotchi/Character.py
+++ Tamagotchi/Character.py
@@ -8,10 +8,14 @@ class Character:
     def __init__(self, sprite: Surface) -> None:
         self.position_x: int = 125
         self.position_y: int = 400
-        self.sprite: Surface = sprite
         self.jump_power: int = 10
         self.jump_count: int = self.jump_power
-        self.is_jump = False
+        self.sprite: Surface = sprite
+        self.is_jump: bool = False
+        self.suit: str = "default"
 
     def change_sprite(self, sprite: Surface) -> None:
         self.sprite = sprite
+
+    def change_suit(self, suitname: str) -> None:
+        self.suit = suitname
--- Tamagotchi/main.py
+++ Tamagotchi/main.py
@@ -23,19 +23,41 @@ def main() -> None:
 
     screen.fill((0, 0, 0))
 
+    label = font.Font(
+        f"{this_module_directory}/assets/font/Kingthings Christmas 2.2.ttf", 40
+    )
+    xmasbundle_button = label.render("Get Christmas Bundle", True, (255, 0, 0))
+    xmasbundle_button_collisionshape = xmasbundle_button.get_rect(topleft=(300, 300))
+
     character = Character(
-        sprite=image.load(f"{this_module_directory}/assets/images/cat.png")
+        sprite=image.load(f"{this_module_directory}/assets/images/cat_default.png")
     )
 
-    background = image.load(
-        f"{this_module_directory}/assets/images/background.jpg"
-    ).convert()
+    special_window = image.load(
+        f"{this_module_directory}/assets/images/special_window.png"
+    )
+    is_show_special_window: bool = False
+
+    background_type = "default"
 
     clock = time.Clock()
 
     while True:
+        background = image.load(
+            f"{this_module_directory}/assets/images/background_{background_type}.jpg"
+        ).convert()
         clock.tick(30)  # set FPS
         mouse = pygame.mouse.get_pressed()
+        mouse_positions = pygame.mouse.get_pos()
+
+        if mouse_positions[0] < 400 and mouse_positions[1] < 100 and mouse[0]:
+            is_show_special_window = True
+
+        elif is_show_special_window and mouse[0]:
+            is_show_special_window = False
+            xmasbundle_button.set_alpha(1)
+            character.change_suit("xmas")
+            background_type = "xmas"
 
         if not character.is_jump:
             if mouse[0]:
@@ -45,17 +67,23 @@ def main() -> None:
                 if character.jump_count > 0:
                     character.position_y -= (character.jump_count**2) / 2
                     character.change_sprite(
-                        sprite=image.load(f"{this_module_directory}/assets/images/cat_jump.png")
+                        sprite=image.load(
+                            f"{this_module_directory}/assets/images/cat_jump_{character.suit}.png"
+                        )
                     )
                 else:
                     character.position_y += (character.jump_count**2) / 2
                     character.change_sprite(
-                        sprite=image.load(f"{this_module_directory}/assets/images/cat_fall.png")
+                        sprite=image.load(
+                            f"{this_module_directory}/assets/images/cat_fall_{character.suit}.png"
+                        )
                     )
                 character.jump_count -= 1
             else:
                 character.change_sprite(
-                    sprite=image.load(f"{this_module_directory}/assets/images/cat.png")
+                    sprite=image.load(
+                        f"{this_module_directory}/assets/images/cat_{character.suit}.png"
+                    )
                 )
                 character.is_jump = False
                 character.jump_count = character.jump_power
@@ -66,6 +94,12 @@ def main() -> None:
 
         screen.blit(background, (0, 0))
         screen.blit(character.sprite, (character.position_x, character.position_y))
+
+        screen.blit(xmasbundle_button, (0, 0))
+
+        if is_show_special_window:
+            screen.blit(special_window, (20, 100))
+
         display.update()
 
 
