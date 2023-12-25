341efdca4d543e98ddfe636a473389b2d812ef5f
AlexNottaBen <alex.nottaben@gmail.com>
2023-12-25 17:09:14 +0200

    Created base structure for Tamagotchi.

--- Tamagotchi/main.py
+++ Tamagotchi/main.py
@@ -1,9 +1,30 @@
 #! /usr/bin/python3
 # -*- coding: utf-8 -*-
 
+import pygame
+from pygame import *
 
-def main():
-    input()
+
+def main() -> None:
+
+    init()
+
+    display.set_caption("Tamagotchi")
+
+    screen_width, screen_height = (600, 800)
+
+    screen = display.set_mode((screen_width, screen_height))
+
+    screen.fill((0, 0, 0))
+
+    background = image.load("./Tamagotchi/assets/images/background.jpg").convert()
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                exit()
+        screen.blit(background, (0, 0))
+        display.update()
 
 
 if __name__ == "__main__":
